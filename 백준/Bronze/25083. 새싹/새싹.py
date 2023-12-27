d9a0aa06ccfcb36d001b69bf8df983acb4d5a849
hyojjin-jeong <64136923+hyojjin-jeong@users.noreply.github.com>
2023-12-27 19:10:43 +0900

    [Bronze V] Title: 새싹, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline  #필수로 해놓기
+
+l1 = "         ,r'" + '"7'
+l2 = "r`-_   ,'  ,/"
+l3 = ' \. ". L_r' + "'"
+l4 = "   `~\/"
+l5 = "      |"
+l6 = "      |"
+print(l1)
+print(l2)
+print(l3)
+print(l4)
+print(l5)
+print(l6)
