79bb6f9dd08d62068874e685fea1d591fa786f41
hyojjin-jeong <64136923+hyojjin-jeong@users.noreply.github.com>
2024-02-02 21:19:42 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 504 ms, Memory: 236432 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+a = input().rstrip()
+b = input().rstrip()
+
+dp = [[0 for _ in range(len(a)+1)] for _ in range(len(b)+1)]
+ans = 0
+for i in range(len(b)+1):
+  for j in range(len(a)+1):
+    if i == 0 or j == 0:
+      dp[i][j] = 0
+    elif b[i-1] == a[j-1]:
+      dp[i][j] = dp[i-1][j-1] + 1
+      ans = max(ans, dp[i][j])
+    else:
+      dp[i][j] = 0
+print(ans)
