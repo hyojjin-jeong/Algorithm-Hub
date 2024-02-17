27ae8fe3fce434084cdbd12b8164a0cd8af62c14
hyojjin-jeong <64136923+hyojjin-jeong@users.noreply.github.com>
2024-02-17 20:48:15 +0900

    [Gold V] Title: 합분해, Time: 48 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N, K = map(int,input().split())
+dp = [[1 for _ in range(N+1)] for _ in range(K)]
+for i in range(1,K):
+  for j in range(1,N+1):
+    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+print(dp[-1][-1] % 1000000000)
