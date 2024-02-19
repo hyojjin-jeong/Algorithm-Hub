e889ac2a3d55d02677a0d5da01840734f9c08c7f
hyojjin-jeong <64136923+hyojjin-jeong@users.noreply.github.com>
2024-02-19 23:21:08 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+li = []
+for _ in range(N):
+  s = list(input().rstrip().split())
+  if s[0] == "push":
+    li.append(int(s[1]))
+  elif s[0] == "pop":
+    if li:
+      print(li.pop(0))
+    else:
+      print(-1)
+  elif s[0] == "size":
+    print(len(li))
+  elif s[0] == "empty":
+    if li:
+      print(0)
+    else:
+      print(1)
+  elif s[0] == "front":
+    if li:
+      print(li[0])
+    else:
+      print(-1)
+  elif s[0] == "back":
+    if li:
+      print(li[-1])
+    else:
+      print(-1)
