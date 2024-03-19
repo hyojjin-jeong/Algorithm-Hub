126c21d2b9b9027fc84f0519300443092495cb4b
hyojjin-jeong <64136923+hyojjin-jeong@users.noreply.github.com>
2024-03-19 20:57:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 39824 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+import heapq #다익스트라 알고리즘, 우선순위 큐 사용
+
+N = int(input())
+q = []
+for _ in range(N):
+  x = int(input())
+  if x == 0:
+    if q:
+      print(heapq.heappop(q)[1])
+    else:
+      print(0)
+  else:
+    heapq.heappush(q, (abs(x),x))
